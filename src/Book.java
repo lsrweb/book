35c545032d4b75ba812fc0c8d97f998147be63fa
Lisa Feaster <61956525+lsrweb@users.noreply.github.com>
2025-03-13 15:58:23 +0800

    1

--- src/Book.java
+++ src/Book.java
@@ -15,27 +15,21 @@ public class Book {
     public void setId(String id) {
         this.id = id;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getAuthor() {
         return author;
     }
-
     public void setAuthor(String author) {
         this.author = author;
     }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
